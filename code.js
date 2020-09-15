a5f02076334ef77ecac5bd11b83894a971c99bcb
Yikuan Sun <yikuansun2015@gmail.com>
2020-09-15 19:13:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -9,3 +9,24 @@ searchbarelem.style.padding = "15px";
 searchbarelem.style.boxSizing = "border-box";
 searchbarelem.placeholder = "Search here";
 document.body.appendChild(searchbarelem);
+
+// update the display
+function draw(query) {
+    // display all items that include the query
+    for (item of searchitems) {
+        if (item.includes(query)) {
+            itemelem = document.createElement("div");
+            itemelem.innerText = item;
+            itemelem.class = "searcheditem";
+            document.body.appendChild(itemelem);
+        }
+    }
+}
+
+// do it ig
+draw("");
+
+// update display everytime bar is changed
+searchbarelem.onkeyup = function() {
+    draw(this.value);
+}
