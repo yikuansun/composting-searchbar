7033630acf9a0b19fa68511767cad1a51d241883
Yikuan Sun <yikuansun2015@gmail.com>
2020-09-15 19:04:25 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,9 @@
 // initiate search items
-document.getElementById("compostableitems").split("\n")
+searchitems = document.getElementById("compostableitems").split("\n");
+
+// add search bar
+searchbarelem = document.createElement("input");
+searchbarelem.type = "text";
+searchbarelem.style.width = "100%";
+searchbarelem.style.padding = "15px";
+document.body.appendChild(searchbarelem);
